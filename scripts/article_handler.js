c2a5e34b52a48f34884111777192e17624cd2d0a
BorwinTheJoe <mateusz.jaltoszuk@gmail.com>
2023-11-11 05:02:04 +0100

    • Added file input. • Limited file input to image files. • Created article_handler.js file and commented in needed steps. •Created user_handler.js file and commented in needed steps.

---
+++ scripts/article_handler.js
@@ -0,0 +1,16 @@
+//Receive Form Inputs
+//Parse Form Inputs into Parts
+//Make Parsed Input into an Article object
+/*(read server data to pick new articleID, Read Date,
+  set Rating to 0, hot to False, Check user base for username,
+  If new username, add new User with placeholder data and new username ID, use new authorId or an existing auithorId)*/
+//Push new_Article object into articles.js array if Valid object.
+//if object invalid -> if new username -> remove placeholder data, remove uploaded image.
+//if push successful, add articleId to author's profile
+
+//export push method for adding an article to the database
+//export method for removing an article from the database
+//import push method for adding users to the database
+//import method for removing users from the database
+//import method for adding articles to author's profiles
+//import method for removing articles from Author's profiles
---
+++ scripts/user_handler.js
@@ -0,0 +1,8 @@
+//export push method for adding users
+//export method for removing users
+//export method for adding articles to user's profiles.
+
+//Add New User Method (with all Required Data)
+//Remove User Method (takes either user ID or username)
+//Add Article to User Method (ArticleID, UserId)
+//Remove Article from User Method (ArticleId, UserId)
